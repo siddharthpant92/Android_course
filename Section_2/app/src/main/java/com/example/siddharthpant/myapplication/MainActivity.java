fd68818fde6961d266681145b1bf5d6e9e35af3d
Siddharth Pant <siddharthpant92@gmail.com>
2018-06-09 17:22:39 -0600

    Section 2 done

--- Section_2/app/src/main/java/com/example/siddharthpant/myapplication/MainActivity.java
+++ Section_2/app/src/main/java/com/example/siddharthpant/myapplication/MainActivity.java
@@ -11,10 +11,12 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "MainActivity";
-    EditText username, password;
+    EditText username, password, currency;
     ImageView image;
 
-    String usernameText, passwordText;
+    String usernameText;
+    String passwordText;
+    Double currencyValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
+        currency = (EditText) findViewById(R.id.currency);
 
         image = (ImageView) findViewById(R.id.imageView);
         image.setImageResource(R.drawable.image1);
@@ -50,5 +53,11 @@ public class MainActivity extends AppCompatActivity {
             image.setImageResource(R.drawable.image1);
             image.setTag(1);
         }
+    }
+
+    public void changeCurrencyTapped(View view)
+    {
+        currencyValue = Double.valueOf(String.valueOf(currency.getText()));
+        Toast.makeText(MainActivity.this, "$"+currencyValue+" = "+String.valueOf(String.format("%.2f",currencyValue*66.32))+"Rupees", Toast.LENGTH_SHORT).show();
     }
 }
