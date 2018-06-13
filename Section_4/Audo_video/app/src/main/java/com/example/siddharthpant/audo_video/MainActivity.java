b9dc5e18edd1d76712027e2e651ffb755568e928
Siddharth Pant <siddharthpant92@gmail.com>
2018-06-12 19:36:50 -0600

    Audio player started

--- Section_4/Audo_video/app/src/main/java/com/example/siddharthpant/audo_video/MainActivity.java
+++ Section_4/Audo_video/app/src/main/java/com/example/siddharthpant/audo_video/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.siddharthpant.audo_video;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
         videoView.setMediaController(mediaController);
 
         videoView.start();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.demoaudio);
+    }
+
+    public void playAudioTapped(View view)
+    {
+        mediaPlayer.start();
+    }
+
+    public void pauseAudioTapped(View view)
+    {
+        mediaPlayer.pause();
     }
 }
