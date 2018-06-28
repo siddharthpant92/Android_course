7fac700b31924fe9abc36ed2ee10d3e6af563945
Siddharth Pant <siddharthpant92@gmail.com>
2018-06-28 12:11:41 -0600

    Shared preferences of menu item

--- Section_7/Shared_Pref_Demo/app/src/main/java/com/example/siddharthpant/shared_pref_demo/MainActivity.java
+++ Section_7/Shared_Pref_Demo/app/src/main/java/com/example/siddharthpant/shared_pref_demo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.siddharthpant.shared_pref_demo;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,15 +18,24 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     String tag = "MainActivity";
+    String langauge1 = "English";
+    String langauge2 = "Hindi";
 
     SharedPreferences sharedPreferences;
+
+    TextView languagePref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        languagePref = (TextView) findViewById(R.id.languagePref);
+
         sharedPreferences = this.getSharedPreferences("com.example.siddharthpant.shared_pref_demo", Context.MODE_PRIVATE);
+        Log.d(tag, String.valueOf(sharedPreferences.getAll()));
+
+        languagePref.setText(sharedPreferences.getString("selectedLanguage", langauge1));
 
         sharedPreferences.edit().putString("username", "Sid").apply();
 
@@ -56,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         Log.d(tag, String.valueOf(test2));
+
+
     }
 
     @Override
@@ -79,11 +93,40 @@ public class MainActivity extends AppCompatActivity {
             case R.id.help:
                 Log.d(tag, "help selected");
                 break;
+            case R.id.change:
+                changeLangauge();
+                break;
             default:
                 Log.d(tag, "default");
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void changeLangauge()
+    {
+        new AlertDialog.Builder(this)
+                        .setTitle("Change your language preference")
+                        .setMessage("Select your language")
+                        .setPositiveButton(langauge1, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                setNewLangauge(langauge1);
+                            }
+                        })
+                        .setNegativeButton(langauge2, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                setNewLangauge(langauge2);
+                            }
+                        })
+                        .show();
+    }
+
+    public void setNewLangauge(String newLangauge)
+    {
+        sharedPreferences.edit().putString("selectedLanguage", newLangauge).apply();
+        languagePref.setText(newLangauge);
     }
 }
