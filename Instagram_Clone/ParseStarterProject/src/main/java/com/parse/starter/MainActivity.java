47d99f8e8fdaced9edbb0310f3d14aab3f565b13
Siddharth Pant <siddharthpant92@gmail.com>
2018-07-04 17:13:32 -0600

    Getting data from parse server

--- Instagram_Clone/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ Instagram_Clone/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,9 +11,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 
@@ -28,19 +30,36 @@ public class MainActivity extends AppCompatActivity {
         //Parse dasboard: http://54.153.48.88/apps/
 
         //Storing object in parse server
-        ParseObject score = new ParseObject("Score"); //Class name
-        score.put("username", "Sid");
-        score.put("score", 85);
-        score.saveInBackground(new SaveCallback() {
+//        ParseObject score = new ParseObject("Score"); //Class name
+//        score.put("username", "Sid");
+//        score.put("score", 85);
+//        score.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if(e == null)
+//                {
+//                    Log.d(tag, "Saved");
+//                }
+//                else
+//                {
+//                    Log.d(tag, "Error: "+e);
+//                }
+//            }
+//        });
+
+        //Getting data from parse server
+        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Score");
+        parseQuery.getInBackground("XAfhxSikka", new GetCallback<ParseObject>() {
             @Override
-            public void done(ParseException e) {
-                if(e == null)
+            public void done(ParseObject object, ParseException e) {
+                if(e == null && object != null)
                 {
-                    Log.d(tag, "Saved");
+                    Log.d(tag, object.getString("username"));
+                    Log.d(tag, String.valueOf(object.getInt("score")));
                 }
                 else
                 {
-                    Log.d(tag, "Error: "+e);
+                    Log.d(tag, "Exception: "+e);
                 }
             }
         });
