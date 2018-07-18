db71e871dfda3af7141a3a6cb46fffc6e92eb0ca
Siddharth Pant <siddharthpant92@gmail.com>
2018-07-17 22:43:23 -0600

    Displaying rider and driver location

--- Uber/ParseStarterProject/src/main/java/com/parse/starter/DriverMapActivity.java
+++ Uber/ParseStarterProject/src/main/java/com/parse/starter/DriverMapActivity.java
@@ -1,15 +1,24 @@
 package com.parse.starter;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.RelativeLayout;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class DriverMapActivity extends FragmentActivity implements OnMapReadyCallback
 {
@@ -34,16 +43,38 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
     {
         mMap = googleMap;
     
-        Bundle bundle = getIntent().getExtras();
-        riderUsername = bundle.getString("riderUsername");
-        riderLatitude = bundle.getDouble("riderLatitude");
-        riderLongitude = bundle.getDouble("riderLongitude");
-        driverLatitude = bundle.getDouble("driverLatitude");
-        driverLongitude = bundle.getDouble("driverLongitude");
-    
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        ConstraintLayout mapLayout = (ConstraintLayout)findViewById(R.id.activityLayout);
+        mapLayout.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
+        {
+            @Override
+            public void onGlobalLayout()
+            {
+                Bundle bundle = getIntent().getExtras();
+                riderUsername = bundle.getString("riderUsername");
+                riderLatitude = bundle.getDouble("riderLatitude", 0);
+                riderLongitude = bundle.getDouble("riderLongitude", 0);
+                driverLatitude = bundle.getDouble("driverLatitude", 0);
+                driverLongitude = bundle.getDouble("driverLongitude", 0);
+            
+                LatLng driverLocation = new LatLng(driverLatitude, driverLongitude);
+                LatLng riderLocation = new LatLng(riderLatitude, riderLongitude);
+            
+                ArrayList<Marker> markers = new ArrayList<>();
+                markers.add(mMap.addMarker(new MarkerOptions().position(driverLocation).title("Driver: You")));
+                markers.add(mMap.addMarker(new MarkerOptions().position(riderLocation).title("Rider: "+riderUsername).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))));
+            
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                for (Marker marker : markers)
+                {
+                    builder.include(marker.getPosition());
+                }
+            
+                LatLngBounds bounds = builder.build();
+                int padding = 100; // offset from edges of the map in pixels
+                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                mMap.animateCamera(cu);
+            
+            }
+        });
     }
 }
